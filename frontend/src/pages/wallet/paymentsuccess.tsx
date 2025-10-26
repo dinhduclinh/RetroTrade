fc8eb288a7996d101775f8e36676fd34ce9055ef
Huyennthe <huyennthe172781@fpt.edu.vn>
2025-10-26 22:58:15 +0700

    user wallet

---
+++ frontend/src/pages/wallet/paymentsuccess.tsx
@@ -0,0 +1,9 @@
+export default function PaymentSuccess() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1> Thanh toán thành công!</h1>
+      <p>Cảm ơn bạn đã nạp tiền vào ví.</p>
+      <a href="/wallet"> Quay lại ví</a>
+    </div>
+  );
+}
