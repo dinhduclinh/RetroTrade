dd2cae3d288b5a3684a147fe6676397e4d4e174e
Huyennthe <huyennthe172781@fpt.edu.vn>
2025-11-10 15:29:31 +0700

    update wallet routers

--- backend/src/routes/wallet/wallet.routes.js
+++ backend/src/routes/wallet/wallet.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getMyWallet, depositToWallet, handlePayOSWebhook, withdrawFromWallet } = require("../../controller/wallet/wallet.Controller");
+const { getMyWallet, depositToWallet, handlePayOSWebhook, withdrawFromWallet, getRecentWalletTransactions, getWalletTransactions } = require("../../controller/wallet/wallet.Controller");
 const { authenticateToken, authorizeRoles } = require("../../middleware/auth");
 const { addBankAccount, getAllBankAccounts, deleteBankAccount } = require("../../controller/wallet/bank.Controler");
-const{ getWithdrawalRequests, reviewWithdrawalRequest, completeWithdrawal, getAllWalletTransactions } = require("../../controller/wallet/admin.Controller");
+const { getWithdrawalRequests, reviewWithdrawalRequest, completeWithdrawal, getAllWalletTransactions, getAdminWallet } = require("../../controller/wallet/admin.Controller");
+const { payment } = require("../../controller/wallet/payment.Controller");
 const router = express.Router();
 
 
@@ -16,8 +17,15 @@ router.delete('/delete/:id', authenticateToken, deleteBankAccount);
 router.post("/withdraw", authenticateToken, withdrawFromWallet);
 
 // Các router chỉ admin  có quyền truy cập
-router.get("/withdrawal-requests", authenticateToken, authorizeRoles("admin"),getWithdrawalRequests);
+router.get("/withdrawal-requests", authenticateToken, authorizeRoles("admin"), getWithdrawalRequests);
 router.put("/withdrawal-requests/:transactionId/review", authenticateToken, authorizeRoles("admin"), reviewWithdrawalRequest);
 router.put("/withdrawal-requests/:transactionId/complete", authenticateToken, authorizeRoles("admin"), completeWithdrawal);
 router.get("/transactions", authenticateToken, authorizeRoles("admin"), getAllWalletTransactions);
+router.get("/admin/wallet", authenticateToken, authorizeRoles("admin"), getAdminWallet);
+//  route thanh toán đơn hàng, chỉ người dùng đã xác thực mới được thanh toán
+router.post("/order/payment", authenticateToken, payment);
+router.get("/transactions/recent", authenticateToken, getRecentWalletTransactions); // Lấy 3 giao dịch gần nhất của ví người dùng
+router.get("/user/transactions", authenticateToken, getWalletTransactions); // Lấy tất cả giao dịch của ví người dùng
+
+
 module.exports = router; 
